62323475890c1dc2aa31810e39b7ceb8779a5d4a
LHJ Piper <2940596912@qq.com>
2021-01-04 11:32:40 +0800

    Find First and Last Position of Element in Sorted Array

---
+++ vsc_leetcode/32.最长有效括号.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=cpp
+ *
+ * [32] 最长有效括号
+ *
+ * https://leetcode-cn.com/problems/longest-valid-parentheses/description/
+ *
+ * algorithms
+ * Hard (34.27%)
+ * Likes:    1124
+ * Dislikes: 0
+ * Total Accepted:    118.9K
+ * Total Submissions: 347K
+ * Testcase Example:  '"(()"'
+ *
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+
+    }
+};
+// @lc code=end
+
