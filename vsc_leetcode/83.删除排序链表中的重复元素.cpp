db4baec5bce8ba64d202ea13e6989fd7a2d279d1
LHJ Piper <2940596912@qq.com>
2021-01-10 12:06:23 +0800

    Remove Duplicates from Sorted List

---
+++ vsc_leetcode/83.删除排序链表中的重复元素.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=cpp
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (51.82%)
+ * Likes:    447
+ * Dislikes: 0
+ * Total Accepted:    171.7K
+ * Total Submissions: 331.3K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ * 
+ */
+
+// @lc code=start
+
+// Definition for singly-linked list.
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *curr = head;
+        while (curr != NULL && curr->next != NULL)
+        {
+            if (curr->val == curr->next->val)
+                curr->next = curr->next->next;
+            else
+                curr = curr->next;
+        }
+        return head;
+    }
+};
+// @lc code=end
